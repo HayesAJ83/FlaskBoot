3c5df9d020bf10cc05c765a7a1e200bace4df60f
HayesAJ83 <72214809+HayesAJ83@users.noreply.github.com>
2021-04-25 11:34:58 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -6,15 +6,13 @@ import pandas as pd
 app = Flask(__name__)
 @app.route('/')
 def index():
+#    url = 'https://raw.githubusercontent.com/HayesAJ83/SurgicalEps_01/main/Eponyms4python_Lite.csv'
+#    df1 = pd.read_csv(url, dtype={'PMID':str,'Year':int})
+#    df2 = df1.sort_values(by=['Eponym'],ascending=True)
+#    table = df2['Eponym_easy']    
     return render_template('index.html')
 
-@app.route('/static/pages/A2Z')
-def index():
-    url = 'https://raw.githubusercontent.com/HayesAJ83/SurgicalEps_01/main/Eponyms4python_Lite.csv'
-    df1 = pd.read_csv(url, dtype={'PMID':str,'Year':int})
-    df2 = df1.sort_values(by=['Eponym'],ascending=True)
-    table = df2['Eponym_easy']
-    return render_template('A2Z.html')
+
 
 if __name__ == "__main__":
     app.run(Debug=True)
