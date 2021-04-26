f196148efb22f1f6655d7c5cc5a5475bcff22fe6
HayesAJ83 <72214809+HayesAJ83@users.noreply.github.com>
2021-04-26 13:09:34 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -1,32 +1,10 @@
 from flask import Flask, render_template
-import pandas as pd
-
 
 app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
-#    url = 'https://raw.githubusercontent.com/HayesAJ83/SurgicalEps_01/main/Eponyms4python_Lite.csv'
-#    df1 = pd.read_csv(url, dtype={'PMID':str,'Year':int})
-#    df2 = df1.sort_values(by=['Eponym'],ascending=True)
-#    data = df2['Eponym_easy'].tolist()   
 
-
-#@app.route('/A2Z')
-#def A2Z():
-#    url = 'https://raw.githubusercontent.com/HayesAJ83/SurgicalEps_01/main/Eponyms4python_Lite.csv'
-#    df1 = pd.read_csv(url, dtype={'PMID':str,'Year':int})
-#    df2 = df1.sort_values(by=['Eponym'],ascending=True)
-#    table = df2['Eponym_easy']
-#    return render_template('A2Z_1.html')
-
-#@app.route('/templates/A2Z_1.html')
-#def A2Z():
-#    url = 'https://raw.githubusercontent.com/HayesAJ83/SurgicalEps_01/main/Eponyms4python_Lite.csv'
-#    df1 = pd.read_csv(url, dtype={'PMID':str,'Year':int})
-#    df2 = df1.sort_values(by=['Eponym'],ascending=True)
-#    data = df2['Eponym_easy'].tolist()
-#    return render_template('A2Z_1.html', data=data)
 
 if __name__ == "__main__":
     app.run(Debug=True)
